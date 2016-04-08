81cdddd36a65e3459f3531d7b8e9547e179348b6
Next <nextacademy@email.com>
2016-04-08 10:37:22 +0800

    completed aws integration, bucket name for profile pic of user is profiler2bucket

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,7 +33,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [20, 20]
+    process :resize_to_fit => [15, 15]
   end
 
   version :medium do
