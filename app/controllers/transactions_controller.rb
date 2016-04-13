6469002e7a9db01943d2e1b1c0593d3691ce9b40
Next <nextacademy@email.com>
2016-04-13 11:28:27 +0800

    Installed payment gateway sucessfully!

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,26 +2,32 @@ class TransactionsController < ApplicationController
 
 
   def create
-    
     @result = Braintree::Transaction.sale(
               amount: Reservation.find(params[:transaction][:reservation_id]).total_price,
               payment_method_nonce: params[:payment_method_nonce])
-    
     if @result.success?
       Transaction.create(reservation_id: params[:transaction][:reservation_id], braintree_transaction_id: @result.transaction.id, status: @result.transaction.status, last_4: @result.transaction.credit_card_details.last_4)
      
       # current_user.purchase_cart_movies!
-      redirect_to reservations_path, notice: "Congratulations! Your transaction has been successfully!"
+      redirect_to reservations_path, notice: "Congratulations! Your transaction has been successfully processed!"
+
     else
-      flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
+      
+      Transaction.create(reservation_id: params[:transaction][:reservation_id], braintree_transaction_id: @result.transaction.id, status: @result.transaction.status, last_4: @result.transaction.credit_card_details.last_4)
+   
       gon.client_token = generate_client_token
-      render :new
+      @reservation = Reservation.find(params[:transaction][:reservation_id])  
+      @transaction = Transaction.new
+      
+      redirect_to reservation_path(@reservation), notice: "Something went wrong while processing your transaction. Please try again!"
     end
   end
 
 
   def new
-      @reservation = Reservation.find(params[:id])
+      if @reservation.blank?
+        @reservation = Reservation.find(params[:id])
+      end
       @client_token= Braintree::ClientToken.generate.to_s
       gon.client_token = generate_client_token
       @transaction = Transaction.new
