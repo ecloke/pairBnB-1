3b47089cbb5622a722ffd61748161818b80ea69b
Next <nextacademy@email.com>
2016-04-15 12:47:22 +0800

    added keys and values to heroku

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV["YOUR AMAZON ACCESS KEY"],
-      :aws_secret_access_key  => ENV["YOUR AMAZON SECRET KEY"],
+      :aws_access_key_id      => ENV["YOUR_AMAZON_ACCESS_KEY"],
+      :aws_secret_access_key  => ENV["YOUR_AMAZON_SECRET_KEY"],
       :region                 => 'ap-southeast-1' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = "profiler2bucket"
